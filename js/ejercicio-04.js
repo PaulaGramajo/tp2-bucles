65921f3a69c4e4c618808c80aceb67c07cb4f9f9
Paula Gramajo <paulagramajo.tuc@gmail.com>
2025-06-18 19:08:27 -0300

    borre un comentario del ejercicio 4

--- js/ejercicio-04.js
+++ js/ejercicio-04.js
@@ -1,19 +1,5 @@
 /*Realiza un script que pida números hasta que se pulse “cancelar”. Si no es un número deberá indicarse con un «alert» y seguir pidiendo números. Al salir con “cancelar” deberá indicarse la suma total de los números introducidos. */
 
-/*let suma = 0;
-do{
-    let num = parseInt(prompt("Ingrese un numero: "))
-    if(num !== null){
-        let numero = Number(num);
-        if(suma === 0){
-            suma = numero;
-        } else{
-            suma = suma + numero
-        }
-    }
-
-} while(confirm('¿Desea ingresar otro número?'));
-document.writeln('La suma de los numeros ingresados es igual a: '+suma);*/
 let suma = 0;
 
 do {
