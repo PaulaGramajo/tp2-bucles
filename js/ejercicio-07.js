406d994019825074e6db02ef097fba3fcaaeeed9
Paula Gramajo <paulagramajo.tuc@gmail.com>
2025-06-19 17:04:29 -0300

    realice el desarrollo del ejercicio 7 del tp de bucles

---
+++ js/ejercicio-07.js
@@ -0,0 +1,27 @@
+/* Haz un script que escriba una pirámide inversa de los números del 1 al número 
+que indique el usuario (no mayor de 50)  de la siguiente forma : (suponiendo que 
+indica 30). 
+ 
+303030303030303030303030303030303030303030303030303030303030 
+2929292929292929292929292929292929292929292929292929292929 
+28282828282828282828282828282828282828282828282828282828 
+….. 
+333 
+22 
+1*/
+
+let piramide = "";
+const num = parseInt(prompt('Ingrese un número:'))
+
+if(num<50){
+for(let i = num; i>=1; i--){
+    let linea = "";
+    for(let j = 0; j<i; j++){
+        linea = linea + i;
+    }
+    piramide = piramide + linea + "<br>"; 
+}
+}else{
+    alert('El número debe ser menor que 50')
+}
+document.writeln(piramide)
